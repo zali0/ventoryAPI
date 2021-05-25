b8922bf7a908fdd5fd0486227e0473e94ae85389
Zeeshan Ali <zali2592@gmail.com>
2021-05-25 23:17:37 +0530

    Modifying CORS settings

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 app.post('/signin', (req,res) => {
     console.log(req.body.password)
