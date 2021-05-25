efb20f10cf7c4f7f56077713c0014d0581f19f3b
Zeeshan Ali <zali2592@gmail.com>
2021-05-25 22:29:47 +0530

    Updating Database

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import knex from 'knex';
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'allah',
-    database : 'test'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
   }
 });
 
